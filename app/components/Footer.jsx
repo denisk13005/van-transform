901f2252f972c7395669654ca0185d4783a6b276
denisk13005 <kirmanndenis@gmail.com>
2024-11-10 17:04:27 +0100

    footer et react icon

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,7 +1,13 @@
+'use client'
+import { useRouter } from "next/navigation";
+import { FaFacebook } from "react-icons/fa";
+import styles from './Footer.module.css';
 export default function Footer() {
+  const router = useRouter()
   return (
-    <footer>
-      <p>&copy; 2024 Transformation de Van en Camping-car</p>
+    <footer className={styles.footerContainer}>
+      <p>&copy; </p>
+      <FaFacebook onClick={() => router.push('https://www.facebook.com/profile.php?id=61554680622442')} />
     </footer>
   );
 }
